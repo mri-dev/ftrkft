749d87ed7820376fc8a0383107d0bb9b3bd61f23
mistvan22 <pocokxtrame@gmail.com>
2017-07-04 15:37:52 +0200

    Save

--- SMARTYAPPBASE/libs/FlexTimeResort/Allasok.php
+++ SMARTYAPPBASE/libs/FlexTimeResort/Allasok.php
@@ -3,7 +3,6 @@ namespace FlexTimeResort;
 
 use ExceptionManager\RedirectException;
 use PortalManager\User;
-use AlertsManager\Alerts;
 
 class Allasok
 {
@@ -18,18 +17,18 @@ class Allasok
   public $controller = null;
   public $smarty = null;
   private $admin = false;
-
   public $settings = array();
 	public $current_page = 1;
   public $total_items = 0;
   public $total_pages = 1;
   public $tree = false;
+
 	private $current_category = false;
 	private $tree_steped_item = false;
 	private $tree_items = 0;
 	private $walk_step = 0;
   private $edit_id = false;
-
+  
   public function __construct($arg = array())
   {
     if ( isset($arg['controller']) ) {
@@ -38,34 +37,25 @@ class Allasok
 			$this->settings = $arg['controller']->settings;
 			$this->smarty = $arg['controller']->smarty;
 		}
-
     if(isset($arg['admin']) && $arg['admin'] === true ){
       $this->admin = true;
     }
-
 		return $this;
   }
-
   public function load( $id, $arg = array() )
   {
     $this->edit_id = $id;
-
     $this->getTree( $arg )->walk();
-
     return $this;
   }
-
   public function creator( $id = false, $data = array() )
   {
     if ( !$id ) {
       // Létrehozás
-
       $updates = array();
       $metas = array();
-
       $updates['author_id'] = $data['author_id'];
       $updates['publish_after'] = (!isset($data['publish_now']) || $data['publish_now']) ? NOW : $data['publish_after'];
-
       $updates['keywords'] = $data['keywords'];
       $updates['megye_id'] = (int)$data['megye_id'];
       $updates['city'] = $data['city'];
@@ -77,7 +67,6 @@ class Allasok
       $updates['author_email'] = (isset($data['author_email']) && !empty($data['author_email'])) ? $data['author_email']:null;
       $updates['city_slug'] = \Helper::makeSafeURL($data['city']);
       $updates['active'] = ($data['active']) ? 1 : 0;
-
       // Metas
       $metas['hirdetes_kategoria'] = array(
         'value' => (int)$data['hirdetes_kategoria'],
@@ -87,7 +76,6 @@ class Allasok
         'value' => (int)$data['hirdetes_tipus'],
         'is_term_list' => 0
       );
-
       if (count($data['munkakorok']['ids']) != 0) {
         foreach ((array)$data['munkakorok']['ids'] as $mid) {
           $metas['munkakorok'][] = array(
@@ -96,29 +84,18 @@ class Allasok
           );
         }
       }
-
       if(!empty($updates)){
         $id = $this->db->insert(
           self::DBTABLE,
           $updates
         );
-
-        if (true) {
-          (new Alerts(array('controller' => $this->controller)))->add(
-            $updates['author_id'],
-            'allas_letrehozas_sikeres',
-            $id
-          );
-        }
       }
-
       $this->rebuildMetas($id, $metas);
       $this->rebuildTermRelations($id, $data['tematic_list']);
     } else {
       // Update
       $updates = array();
       $metas = array();
-
       $updates['keywords'] = $data['keywords'];
       $updates['megye_id'] = (int)$data['megye_id'];
       $updates['city'] = $data['city'];
@@ -130,7 +107,6 @@ class Allasok
       $updates['author_email'] = (isset($data['author_email']) && !empty($data['author_email'])) ? $data['author_email']:null;
       $updates['city_slug'] = \Helper::makeSafeURL($data['city']);
       $updates['active'] = ($data['active']) ? 1 : 0;
-
       // Metas
       $metas['hirdetes_kategoria'] = array(
         'value' => (int)$data['hirdetes_kategoria'],
@@ -140,7 +116,6 @@ class Allasok
         'value' => (int)$data['hirdetes_tipus'],
         'is_term_list' => 0
       );
-
       if (count($data['munkakorok']['ids']) != 0) {
         foreach ((array)$data['munkakorok']['ids'] as $mid) {
           $metas['munkakorok'][] = array(
@@ -149,7 +124,6 @@ class Allasok
           );
         }
       }
-
       if(!empty($updates)){
         $this->db->update(
           self::DBTABLE,
@@ -157,42 +131,32 @@ class Allasok
           sprintf("ID = %d", $id)
         );
       }
-
       $this->rebuildMetas($id, $metas);
       $this->rebuildTermRelations($id, $data['tematic_list']);
     }
-
     return $id;
   }
-
   public function checkRequestAd($uid, $adid)
   {
     $adid = (int)$adid;
     $uid = (int)$uid;
-
     if ($uid == 0 || $adid == 0) {
       return false;
     }
-
     $check = $this->db->query("SELECT hashkey FROM ".self::DB_REQUEST_X." WHERE allas_id = {$adid} and user_id = {$uid}")->fetchColumn();
-
     return (!empty($check)) ? $check : false;
   }
-
   public function getRequestHashkey($uid, $adid)
   {
     $adid = (int)$adid;
     $uid = (int)$uid;
-
     $data = $this->db->query("SELECT
       r.hashkey
     FROM ".self::DB_REQUEST_X." as r
     WHERE r.user_id = '{$uid}' and r.allas_id = {$adid}
     ")->fetchColumn();
-
     return $data;
   }
-
   public function getRequest($hashkey)
   {
     $request = $this->db->query("SELECT
@@ -202,21 +166,16 @@ class Allasok
     LEFT OUTER JOIN admin as a ON a.ID = r.admin_accept_id
     WHERE r.hashkey = '{$hashkey}'
     ")->fetch(\PDO::FETCH_ASSOC);
-
     return $request;
   }
-
   public function requestAd($uid, $adid)
   {
     $adid = (int)$adid;
     $uid = (int)$uid;
-
     if ($uid == 0 || $adid == 0) {
       return false;
     }
-
     $check = $this->checkRequestAd( $uid, $adid );
-
     if( !$check ){
       $hashkey = uniqid();
       $this->db->insert(
@@ -227,13 +186,11 @@ class Allasok
             'allas_id' => $adid,
           )
       );
-
       return $hashkey;
     } else {
       return true;
     }
   }
-
   public function logVisit($uid = false)
   {
     $this->db->insert(
@@ -245,20 +202,16 @@ class Allasok
         )
     );
   }
-
   public function rebuildTermRelations( $id, $terms = array() )
   {
     $insert = array();
-
     if (!empty($terms)) {
       $this->db->query("DELETE FROM ".self::DB_TERM_RELATIONS." WHERE allas_id = {$id};");
       $this->db->query("DELETE FROM ".self::DB_TERM_RELATIONS_ITEM." WHERE allas_id = {$id};");
     }
-
     $gindex = 0;
     foreach ((array)$terms as $term) {
       $term_id = $this->db->query("SELECT ID FROM ".\PortalManager\Categories::DB_LIST." WHERE termkey = '{$term[value]}'")->fetchColumn();
-
       $last_insert_xtermid = $this->db->insert(
         self::DB_TERM_RELATIONS,
         array(
@@ -268,9 +221,7 @@ class Allasok
           'sortindex' => $gindex
         )
       );
-
       $title = ($term['title']) ? $term['title'] : null;
-
       $index = 0;
       foreach ((array)$term['selectedValues'] as $seli) {
         $insert[] = array($last_insert_xtermid, (int)$seli, $id, $index);
@@ -278,7 +229,6 @@ class Allasok
       }
       $gindex++;
     }
-
     if(!empty($insert)) {
       $this->db->multi_insert(
         self::DB_TERM_RELATIONS_ITEM,
@@ -288,15 +238,12 @@ class Allasok
       );
     }
   }
-
   private function rebuildMetas($id, $metas = array())
   {
     $insert = array();
-
     if (!empty($metas)) {
       $this->db->query("DELETE FROM ".self::DB_META." WHERE allas_id = {$id};");
     }
-
     foreach ((array)$metas as $kulcs => $meta) {
       if (is_array($meta[0])) {
         foreach ((array)$meta as $m) {
@@ -306,7 +253,6 @@ class Allasok
         $insert[] = array($kulcs, $meta['value'], $meta['is_term_list'], $id);
       }
     }
-
     if(!empty($insert)) {
       $this->db->multi_insert(
         self::DB_META,
@@ -316,12 +262,10 @@ class Allasok
       );
     }
   }
-
   public function getTree( $arg = array() )
 	{
 		$tree = array();
     $filters = $arg['filters'];
-
 		$qry = "
 			SELECT SQL_CALC_FOUND_ROWS
         a.*,
@@ -331,23 +275,18 @@ class Allasok
 			FROM ".self::DBTABLE." as a
       LEFT OUTER JOIN accounts as u ON u.ID = a.author_id
 			WHERE 1=1";
-
     if ( !$this->admin ) {
         $qry .= " and a.active = 1";
     }
-
     if ( $this->edit_id !== false ) {
         $qry .= " and a.ID = ".$this->edit_id;
     }
-
     if (isset($arg['author_id'])) {
       $qry .= " and a.author_id = ".(int)$arg['author_id'];
     }
-
     if (isset($arg['active_in']) && is_array($arg['active_in'])) {
       $qry .= " and a.active IN(".implode(",", $arg['active_in']).")";
     }
-
     if(isset($arg['show_history'])){
       $idset_orderby = array();
       if($arg['show_history'] === true) {
@@ -357,23 +296,16 @@ class Allasok
         $history_row = 'user_id';
         $hval = (int)$arg['show_history'];
       }
-
       $idset_orderby = $this->getVisitedIDS($history_row, $hval);
-
       $qry .= " and a.ID IN (".implode(",",$idset_orderby).")";
     }
-
     if(isset($arg['show_requests'])){
       $idset_orderby = array();
-
       $history_row = 'user_id';
       $hval = (int)$arg['show_requests'];
-
       $idset_orderby = $this->getRequestsIDS($history_row, $hval);
-
       $qry .= " and a.ID IN (".implode(",",$idset_orderby).")";
     }
-
     // Filterek
     if (isset($filters) && !empty($filters)) {
       // Város
@@ -381,7 +313,6 @@ class Allasok
         $city = \Helper::makeSafeURL(trim($filters['city']));
         $qry .= " and a.city_slug = '$city'";
       }
-
       // Megye
       if ($filters['megye']) {
         if (is_array($filters['megye'])) {
@@ -391,24 +322,18 @@ class Allasok
           $megye = $filters['megye'];
           $qry .= " and a.megye_id = $megye";
         }
-
       }
-
       // Keresés
       if ($filters['search']) {
         $keywords = explode(" ", trim($filters['search']));
         $search_qry = '';
-
         foreach ((array)$keywords as $key) {
           $key = trim($key);
           $search_qry .= "(a.keywords LIKE '%".$key."%' or a.short_desc LIKE '%".$key."%') or ";
         }
-
         $search_qry = rtrim($search_qry, ' or ');
-
         $qry .= ' and ('.$search_qry.')';
       }
-
       // Meták
       if (isset($filters['meta'])) {
         foreach ((array)$filters['meta'] as $key => $value) {
@@ -421,41 +346,31 @@ class Allasok
         }
       }
     }
-
     if (isset($arg['active_in']) && is_array($arg['active_in'])) {
       $qry .= " and a.active IN(".implode(",", $arg['active_in']).")";
     }
-
 		if( !$this->o['orderby'] ) {
       if ($idset_orderby) {
         $qry .= " ORDER BY FIELD(a.ID, ".implode(",", (array)$idset_orderby).")";
       } else {
         $qry .= " ORDER BY a.publish_after DESC";
       }
-
 		} else {
 			$qry .= " ORDER BY a.".$this->o['orderby']." ".$this->o['order'];
 		}
-
     // Limit
 		$limit = $this->getLimit($arg);
 		$qry .= " LIMIT ".$limit[0].", ".$limit[1];
-
     //echo $qry;
-
 		$top_cat_qry 	= $this->db->query($qry);
 		$top_cat_data 	= $top_cat_qry->fetchAll(\PDO::FETCH_ASSOC);
-
     $this->total_items 	= $this->db->query("SELECT FOUND_ROWS();")->fetchColumn();
 		$this->total_pages 	= ceil( $this->total_items / $limit[1] );
-
 		if( $top_cat_qry->rowCount() == 0 ) return $this;
-
 		foreach ( $top_cat_data as $top_cat ) {
 			$this->tree_items++;
       $top_cat['metas'] = $this->loadMetas($top_cat['ID']);
       $top_cat['term_list'] = $this->loadTermList($top_cat['ID']);
-
       if($top_cat['metas'])
       foreach ($top_cat['metas'] as $meta) {
         if($meta['kulcs'] == 'munkakorok') {
@@ -469,90 +384,68 @@ class Allasok
           $top_cat[$meta['kulcs']] = $meta['value'];
         }
       }
-
       $top_cat['tipus_name'] = $this->getTermName($top_cat['hirdetes_tipus']);
       $top_cat['cat_name'] = $this->getTermName($top_cat['hirdetes_kategoria']);
       $top_cat['megye_name'] = $this->getTermName($top_cat['megye_id']);
-
 			$this->tree_steped_item[] = $top_cat;
 			$tree[] = $top_cat;
 		}
-
 		$this->tree = $tree;
-
 		return $this;
 	}
-
   public function getVisitedIDS($by = 'ID', $val = 0, $limit = 10)
   {
     $ids = array();
-
     $set = $this->db->query("
     SELECT
       v.allas_id
     FROM ".self::DB_LOG_VIEW." as v
     WHERE v.{$by} = '{$val}'
     ORDER BY v.visit_at DESC")->fetchAll(\PDO::FETCH_ASSOC);
-
     foreach ((array)$set as $s) {
       if(count($ids) >= $limit) continue;
       if(!in_array((int)$s['allas_id'], $ids)){
         $ids[] = (int)$s['allas_id'];
       }
     }
-
     return $ids;
   }
-
   public function getRequestsIDS($by = 'user_id', $val = 0)
   {
     $ids = array();
-
     $set = $this->db->query("
     SELECT
       v.allas_id
     FROM ".self::DB_REQUEST_X." as v
     WHERE v.{$by} = '{$val}'
     ORDER BY v.request_at DESC")->fetchAll(\PDO::FETCH_ASSOC);
-
     foreach ((array)$set as $s) {
       if(!in_array((int)$s['allas_id'], $ids)){
         $ids[] = (int)$s['allas_id'];
       }
     }
-
     return $ids;
   }
-
   private function getLimit( $arg = array() )
 	{
 		$limit = array( 0, 25 );
-
 		if( isset($arg['limit']) ) {
 			$limit[1] = $arg['limit'];
 		}
-
 		$page = $arg['page'];
-
 		if( isset($page) && $page > 0 ) {
-
 		} else {
 			$page = 1;
 		}
-
 		$limit[0] = $limit[1] * $page - $limit[1];
-
 		$this->limit[0] = $limit[0] + 1;
 		$this->limit[1] = $limit[0] + $limit[1];
 		$this->current_page = $page;
-
 		return $limit;
 	}
-
   private function loadTermList( $adid )
   {
     $data = array();
-
     $terms = $this->db->query("
     SELECT
       tr.title,
@@ -571,10 +464,8 @@ class Allasok
     t.allas_id = {$adid}
     ORDER BY tr.sortindex ASC, t.sortindex ASC
     ")->fetchAll(\PDO::FETCH_ASSOC);
-
     foreach ($terms as $term) {
       $tid = (int)$term['term_id'];
-
       $data[$tid]['value_texts'][] = $term['value_name'];
       $data[$tid]['term_ids'][] = (int)$term['value_id'];
       $data[$tid]['ID'] = $tid;
@@ -585,20 +476,16 @@ class Allasok
         'name' => $term['value_name']
       );
     }
-
     return $data;
   }
-
   private function loadMetas( $adid )
   {
     $metas = array();
-
     $datas = $this->db->query("SELECT
       m.ID, m.kulcs, m.value, m.is_term_list, t.neve as value_text
     FROM ".self::DB_META." as m
     LEFT OUTER JOIN ".\PortalManager\Categories::DBTERMS." as t ON t.ID = m.value
     WHERE m.allas_id = '{$adid}'")->fetchAll(\PDO::FETCH_ASSOC);
-
     foreach ((array)$datas as $d) {
       $metas[$d['ID']] = array_map(function($d){
         $d = trim($d);
@@ -608,14 +495,171 @@ class Allasok
         return $d;
       },$d);
     }
-
     return $metas;
   }
-
   public function Count()
 	{
 		return $this->tree_items;
 	}
-
   public function walk()
 	{
+		if( !$this->tree_steped_item ) return false;
+		$this->current_category = $this->tree_steped_item[$this->walk_step];
+		$this->walk_step++;
+		if ( $this->walk_step > $this->tree_items ) {
+			// Reset Walk
+			$this->walk_step = 0;
+			$this->current_category = false;
+			return false;
+		}
+		return true;
+	}
+  private function getTermName($id = false)
+  {
+    if (!$id) {
+      return null;
+    }
+    $value = $this->db->query("SELECT neve FROM terms WHERE id = {$id}")->fetchColumn();
+    return $value;
+  }
+  public function prepareOutput()
+  {
+    $this->current_category['tipus_name'] = $this->getTermName($this->getTypeID());
+    $this->current_category['cat_name'] = $this->getTermName($this->getCatID());
+    $this->current_category['megye_name'] = $this->getTermName($this->getMegyeID());
+  }
+  /*===============================
+	=            GETTERS            =
+	===============================*/
+  public function get( $key = false )
+	{
+    if ($key) {
+      return $this->current_category[$key];
+    } else {
+      $this->prepareOutput();
+      return $this->current_category;
+    }
+	}
+  public function getMetas($by = false, $byval = false)
+  {
+    $metas = $this->current_category[metas];
+    $re = array();
+    if( $by === false ) return $metas;
+    foreach ((array)$metas as $key => $value) {
+      if($value[$by] != $byval ) continue;
+      $re[$key] = $value;
+    }
+    return $re;
+  }
+  public function getTerms()
+  {
+    return $this->current_category['term_list'];
+  }
+  public function getKeywords( $arrayed = true )
+  {
+    if ($arrayed) {
+      $arr = array();
+      $keys = explode(",", $this->get('keywords'));
+      foreach ((array)$keys as $key) {
+        $arr[] = trim($key);
+      }
+      return $arr;
+    } else {
+      return $this->get('keywords');
+    }
+  }
+	public function getID()
+	{
+		return $this->current_category['ID'];
+	}
+  public function getTypeID()
+  {
+    return $this->current_category['hirdetes_tipus'];
+  }
+  public function getMegyeID()
+  {
+    return $this->current_category['megye_id'];
+  }
+  public function getCatID()
+  {
+    return $this->current_category['hirdetes_kategoria'];
+  }
+  public function shortDesc()
+  {
+    return $this->current_category['short_desc'];
+  }
+  public function getPreContent()
+  {
+    return $this->current_category['pre_content'];
+  }
+  public function getContent()
+  {
+    return $this->current_category['content'];
+  }
+  public function getPublishDate()
+  {
+    return date('Y. m. d.', strtotime($this->current_category['publish_after']));
+  }
+  public function getMegye()
+  {
+    return $this->current_category['megye_name'];
+  }
+  public function getCity()
+  {
+    return $this->current_category['city'];
+  }
+  public function getAuthorData( $key = 'name')
+  {
+    switch ($key) {
+      case 'name':
+        $alt = $this->current_category['author_name'];
+        if (is_null($alt)) {
+          return $this->current_category['oauthor_name'];
+        } else return $alt;
+      break;
+      case 'email':
+        $alt = $this->current_category['author_email'];
+        if (is_null($alt)) {
+          return $this->current_category['oauthor_email'];
+        } else return $alt;
+      break;
+      case 'phone':
+        $alt = $this->current_category['author_phone'];
+        if (is_null($alt)) {
+          return $this->current_category['oauthor_phone'];
+        } else return $alt;
+      break;
+      case 'author':
+        $obj = new User($this->get('author_id'), array('controller' => $this));
+        return $obj;
+      break;
+    }
+  }
+  public function getURL()
+  {
+    $SEO = '';
+    $SEO .= \Helper::makeSafeURL($this->getCity(),'');
+    $SEO .= '/'.\Helper::makeSafeURL($this->getAuthorData('name'),'');
+    return $this->settings['allas_page_slug'] . $SEO . '_'.$this->getID();
+  }
+  private function kill( $msg = '' )
+	{
+		throw new \Exception( $msg . ' ('.__FILE__.')' );
+	}
+	private function error( $msg )
+	{
+		throw new RedirectException( $msg, $_POST['form'], $_POST['return'], $_POST['session_path'] );
+	}
+  public function __destruct()
+	{
+		$this->db = null;
+		$this->smarty = false;
+		$this->settings = null;
+		$this->controller = null;
+    $this->tree = false;
+		$this->tree_steped_item = false;
+		$this->tree_items = 0;
+		$this->walk_step = 0;
+	}
+}
+?>
